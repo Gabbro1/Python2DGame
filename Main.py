e0d5da59a1d78f37ae9676bfd93248b37fb2036b
Gabbro <110852761+TTC-Gabbro@users.noreply.github.com>
2022-09-25 21:03:20 +0200

    first commit

---
+++ Main.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.locals import *
+from pygame import mixer
+
+white = (250, 250, 250)
+black = (0, 0, 0)
+red = (250, 0, 0)
+
+#Initialize the window
+pygame.init()
+mixer.init()
+mixer.music.load('')
+mixer.music.play()
+#Create a screen
+window = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("PLACEHOLDER")
+
+color = red
+  
+# Changing surface color
+window.fill(color)
+pygame.display.flip()
+
+running = True
+
+#Game Loop
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
